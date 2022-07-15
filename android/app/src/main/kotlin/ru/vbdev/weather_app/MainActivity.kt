e35b3263abecf09c8242fe22fc6a79b0f7c89ec4
vbdevru <vbdevru@mail.ru>
2022-07-15 12:32:54 +0300

    #0 Init

---
+++ android/app/src/main/kotlin/ru/vbdev/weather_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package ru.vbdev.weather_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Home());
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Text('Weather App'),
+      ),
+    );
+  }
+}
